0bca6da185a83c9731df943014021ccda1dea001
ren <reignerdiazgutierrez@gmail.com>
2019-09-01 18:20:00 +0800

    added test.py to test the app

---
+++ test.py
@@ -0,0 +1,29 @@
+import modules.config as config
+
+print("Form Labels")
+print(config.form_labels)
+print("\n")
+
+print("Form Shape")
+print(config.form_shape)
+print("\n")
+
+print("Field Join")
+print(config.field_join)
+print("\n")
+
+print("Number Offset")
+print(config.number_offset)
+print("\n")
+
+print("Column Names")
+print(config.column_names)
+print("\n")
+
+print("Item Numbers")
+print(config.item_numbers)
+print("\n")
+
+print("Form Dict")
+print(config.form_dict)
+print("\n")
